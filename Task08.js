d7c23cdeffdd5924d74d2c5c9c71982ed5cb0b81
unknown <Tseholoba2@gmail.com>
2021-05-27 02:44:54 +0200

    latest update

--- Task08.js
+++ Task08.js
@@ -1,14 +1,14 @@
-function toHoursAndMin(num){
+function toHoursAndMin(num) {
     let hours = parseInt(num / 60);
     let minutes = num % 60;
-    if(hours == 1 && minutes == 1){
-    return `${hours} hour and ${minutes} minute`;
-    }else if(hours == 1){
-        return `${hours} hour and ${minutes} minutes`;
-    }else if(minutes == 1){
-        return `${hours} hours and ${minutes} minute`;
-    }else{
-        return `${hours} hours and ${minutes} minutes`;
+    if (hours == 1 && minutes == 1) {
+        return `${hours} hour, ${minutes} minute`;
+    } else if (hours == 1) {
+        return `${hours} hour, ${minutes} minutes`;
+    } else if (minutes == 1) {
+        return `${hours} hours, ${minutes} minute`;
+    } else {
+        return `${hours} hours, ${minutes} minutes`;
     }
 }
 console.log(toHoursAndMin(121));
--- Task09.js
+++ Task09.js
@@ -1,14 +1,11 @@
-function vowels(string){
+function vowels(string) {
     let vowels = '';
-    for(let i = 0 ; i < string.length;i++){
-        if(string[i] == "a" || string[i] == "e" || string[i] == "i" || string[i] == "o" || string[i] == "u" ){
-            vowels+= string[i] + ",";
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] == "a" || string[i] == "e" || string[i] == "i" || string[i] == "o" || string[i] == "u") {
+            vowels += string[i] + ", ";
         }
     }
-    let characters = '';
-    for(let i = 0;i < vowels.length - 1;i++){
-        characters += vowels[i];
-    }
-   console.log(characters); 
- }
- vowels("Umuzi");
+    vowels = vowels.slice(0, vowels.length - 2);
+    console.log(vowels);
+}
+vowels("Umuzi");
