cb8607a796934cfe9e62a209a72edddfa71b4d75
unknown <Tseholoba2@gmail.com>
2021-05-31 11:33:25 +0200

    Updated task06

--- Task06.js
+++ Task06.js
@@ -1,12 +1,12 @@
-function maximum(...num){
+function maximum(...num) {
     let max = num[0];
-    for(let i = 0 ; i < num.length;i++){
-        if(max < num[i]){
+    for (let i = 0; i < num.length; i++) {
+        if (max < num[i]) {
             max = num[i];
         }
     }
- 
-        return max;
-    
+
+    return max;
+
 }
 console.log(maximum(-1, -100, -1000, -88));
