fe2cf95b3d0b5391232d8d60a974653e85d8379d
unknown <Tseholoba2@gmail.com>
2021-06-01 12:50:45 +0200

    Task09 update

--- Task09.js
+++ Task09.js
@@ -1,7 +1,8 @@
 function vowels(string) {
+    string = string.toLowerCase();
     let vowels = '';
     for (let i = 0; i < string.length; i++) {
-        if (string[i] == "a" || string[i] == "e" || string[i] == "i" || string[i] == "o" || string[i] == "u") {
+        if ((string[i] == "a" || string[i] == "e" || string[i] == "i" || string[i] == "o" || string[i] == "u") && !vowels.includes(string[i])) {
             vowels += string[i] + ", ";
         }
     }
